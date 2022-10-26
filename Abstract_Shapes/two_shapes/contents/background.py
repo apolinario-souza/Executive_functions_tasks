82b7b21fcbba6d9b450ef787841ebd917d3e2d1e
Tércio Apolinário-Souza <40078879+apolinario-souza@users.noreply.github.com>
2022-10-26 16:15:36 -0300

    Add files via upload

---
+++ Abstract_Shapes/two_shapes/contents/background.py
@@ -0,0 +1,155 @@
+import pygame
+from .constants import WIDTH, HEIGHT, N_TRIALS, BLACK  
+import random
+
+import numpy as np
+
+
+
+pygame.init()
+
+class Target_random:
+    def __init__(self):
+        self.cont1 = 0
+        self.cont2 = 0
+       
+        
+        self.response = []
+        
+    def target_control (self, n_trial):        
+        for k in range(n_trial):   
+            
+            check = True
+            while check:
+                
+                b = random.randint(1,  2)           
+                if b == 1 and self.cont1 < n_trial//2:
+                    self.response.append(1)
+                    self.cont1 +=1
+                    check = False
+                if b == 2 and self.cont2 < n_trial//2:
+                    self.response.append(2)
+                    self.cont2 +=1
+                    check = False
+               
+                
+                    
+        return self.response
+  
+
+class Background:
+    def __init__(self):
+        self.clicked = False
+        self.current_time = []
+        self.key = []
+        self.trial = 0
+        self.target = []
+        self.cont = 0 #Timer counter        
+        self.sequence = Target_random().target_control(N_TRIALS) 
+        
+        np.savetxt('trials/sequence.csv', np.array(self.sequence))         
+    
+    def draw_rectangle_init (self, win):
+        rect1 = pygame.Rect(0, 0, WIDTH, HEIGHT)
+        rect2 = pygame.Rect(0, 0, WIDTH*.80, HEIGHT*0.4)
+        rect2.center = rect1.center
+        size_cross = int(WIDTH*.035)
+        width = int(WIDTH*.0037)
+        pygame.draw.rect(win, (0, 0, 0), rect1, 2)
+        pygame.draw.rect(win, (255, 255, 255), rect2)
+        
+    
+    def img (self, win, pos_x, pos_y, diameter,  img):
+        image = pygame.image.load(img)   
+        image = pygame.transform.scale(image, (diameter, diameter)) 
+        win.blit(image, (pos_x, pos_y)) 
+    
+    
+    def text_write (self, win, text, width, height):
+        base_font = pygame.font.Font(None, 32)
+        text_surface = base_font.render(text, True, (255,255,255))
+        win.blit(text_surface, (width, height))     
+        
+    def draw_button (self, win):
+        win.fill(BLACK)
+        self.img (win,  WIDTH*.30, 0, WIDTH*.50, "img/inicio.png")        
+        if pygame.key.get_pressed()[pygame.K_SPACE] == True:
+            self.clicked = True        
+        
+           
+      
+            
+    def draw(self, win):
+        
+        
+        
+        
+
+        if self.clicked == True and self.trial < N_TRIALS:
+            #Hide mouse 
+            pygame.mouse.set_visible(False)
+        
+            #def cont timer
+            self.current_time.append(pygame.time.get_ticks())            
+            self.cont = self.current_time[-1] - self.current_time[0]
+            
+            if pygame.key.get_pressed()[pygame.K_RIGHT] == True:
+                self.key.append(1)
+            elif pygame.key.get_pressed()[pygame.K_LEFT] == True:
+                self.key.append(0)
+            else:
+                self.key.append(-1)
+                               
+            
+            # Screen 1: Holding 
+            if self.cont >= 0 and self.cont < 500:
+                win.fill(BLACK)
+                self.draw_rectangle_init(win)                             
+            
+            # Screen 2: Stimulus 
+            if self.cont >= 500 and self.cont < 500+750:
+                win.fill(BLACK)
+                self.draw_rectangle_init(win)
+                
+                                            
+               
+                if self.sequence[self.trial] == 1:
+                    self.img (win,  WIDTH*.45, (HEIGHT//2)-(WIDTH*.05),WIDTH*.10, "img/left1.png") #shape left 1                
+                if self.sequence[self.trial] == 2:
+                    self.img (win,  WIDTH*.45, (HEIGHT//2)-(WIDTH*.05), WIDTH*.10, "img/right1.png")# shape right1
+                                       
+                
+            # Set all ans Save
+            if self.cont >= 500+751 and self.cont >= 500+751+50:
+                win.fill(BLACK)
+                self.draw_rectangle_init(win)
+                names = ['Time', 'Key_press', 'Stimulus']
+                time = []
+                for i in range (len(self.current_time)):
+                    self.target.append(self.sequence[self.trial])
+                    time.append(5*i)
+                   
+                np.savetxt('trials/trial_'+ str(self.trial)+'.csv', np.column_stack([time,self.key, self.target] ), header = ','.join(names), delimiter=',')
+                self.trial +=1                         
+                #Set data
+                self.current_time = []
+                self.key = []
+                self.target = []         
+                             
+        
+        else:
+            
+            pygame.mouse.set_visible(True)
+            
+            
+            self.draw_button (win)
+           
+            
+            
+            
+            
+            
+                
+              
+        
+ 
